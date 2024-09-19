dfbf574d68a868d23deaba7acb8b9a5dc78f0ef7
unknown <tsvetkovprelude@gmail.com>
2024-09-19 11:59:55 +0300

    refactor of char counts

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,5 +1,4 @@
 use itertools::Itertools;
-use std::collections::HashSet;
 
 fn main() {
     let input: Vec<&str> = include_str!("../../data/day2.txt").lines().map(|str| str.trim()).collect();
@@ -7,29 +6,17 @@ fn main() {
     let mut all_twos = 0; 
     let mut all_threes = 0; 
 
+
     for item in &input {
-        let mut twos = 0; 
-        let mut threes = 0; 
-        let unique = item.chars().collect::<HashSet<char>>();
-
-        for ch in unique {
-            let char_count = count_char_occurrences(item, ch);
-            if char_count == 2 {
-                twos += 1;
-            } else if char_count == 3 {
-                threes += 1;
-            }
-        }
-
-        if twos > 0 {
+        let counts = item.chars().counts();
+        if counts.values().any(|&count| count == 2) {
             all_twos += 1;
         }
-
-        if threes > 0 {
+        if counts.values().any(|&count| count == 3) {
             all_threes += 1;
         }
-
     }
+
 
     for combination in input.iter().combinations(2) {
         if count_differing_chars(combination[0], combination[1]) == 1 {
@@ -39,10 +26,6 @@ fn main() {
     }
 
     println!("cheskum: {}", all_twos * all_threes);
-}
-
-fn count_char_occurrences(string: &str, target: char) -> usize {
-    string.chars().filter(|&c| c == target).count()
 }
 
 fn count_differing_chars(s1: &str, s2: &str) -> usize {
