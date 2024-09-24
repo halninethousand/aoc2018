4964c7566fcccc62f01868c9270a18efb8af923f
unknown <tsvetkovprelude@gmail.com>
2024-09-24 12:09:45 +0300

    day5 rework double pointer to stack for polymer reaction

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,49 +1,46 @@
 fn main() {
-    let mut input: Vec<char> = include_str!("../../data/day5_short.txt").trim().chars().collect(); 
-    println!("{input:?}");
+    let input: Vec<char> = include_str!("../../data/day5.txt").trim().chars().collect();
+    
+    let alphabet = (b'a'..=b'z').map(|c| c as char);
+    let mut shortest_length = usize::MAX;
+    let mut remove_char: Option<char> = None;
 
-    let mut annihilated = 0;
-    let mut i = 0;
-    let mut j = 1;
-    let original_length = input.len();
+    // part 1
+    println!("reacted polymer length: {}", polymer(input.clone()).len());
 
-    loop {
-        let length = input.len();
+    // part 2
+    for unit in alphabet {
+        let filtered_input: Vec<char> = input
+            .iter()
+            .cloned()
+            .filter(|&c| c != unit && c != unit.to_ascii_uppercase())
+            .collect();
 
-        if i >= length || j >= length {
-            break;
+        let reacted_length = polymer(filtered_input).len();
+
+        if reacted_length < shortest_length {
+            shortest_length = reacted_length;
+            remove_char = Some(unit);
         }
-
-        let char1 = input[i];
-        let char2 = input[j];
-        let first = char1 as u8;
-        let second = char2 as u8;
-        
-
-
-        if char1.is_uppercase() && char2.is_lowercase() && (second as i8 - first as i8 == 32) {
-            // println!("annihilated: {} -> {}, {} -> {}", char1, first, char2, second); 
-            input.remove(j);
-            input.remove(i);
-            i = 0;
-            j = 1;
-            annihilated += 2;
-        } else if char1.is_lowercase() && char2.is_uppercase() && (first as i8 - second as i8 == 32) {
-            // println!("annihilated: {} -> {}, {} -> {}", char1, first, char2, second); 
-            input.remove(j);
-            input.remove(i);
-            i = 0;
-            j = 1;
-            annihilated += 2;
-        } else {
-            i += 1;
-            j += 1;
-        }
-
-
     }
 
-    println!("elements left: {}", original_length - annihilated);
+    println!("shortest polymer length: {} for {}", shortest_length, remove_char.unwrap());
+}
 
+fn polymer(polymer: Vec<char>) -> String {
+    let mut stack = Vec::new();
 
+    for c in polymer {
+        if let Some(&last) = stack.last() {
+            if c != last && c.to_ascii_lowercase() == last.to_ascii_lowercase() {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        } else {
+            stack.push(c);
+        }
+    }
+
+    stack.into_iter().collect()
 }
